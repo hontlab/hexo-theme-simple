c74083cd4d051d97dc5f5f7ccf82389ecb885425
hontlab <hontlab@gmail.com>
2018-04-17 00:45:38 +0900

    top-btn

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -80,6 +80,9 @@
 }(jQuery))
 
 /* external-link */
-$('.post-main-content a').append('<i class="fa fa-external-link" aria-hidden="true"></i>');
+$('.post-main-content a[target="_blank"]').append('<i class="fa fa-external-link" aria-hidden="true"></i>');
+
+/* book-image */
+$('.post-main-content img').parent().css('text-align', 'center');
 
 
